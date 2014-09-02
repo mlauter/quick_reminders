e73d8dd27aa39cce1cc4ca588ded2ac8c7f92d1a
mlauter <lauter.miriam@gmail.com>
2014-09-02 17:40:05 -0400

    starting to add firebase

--- src/bg/background.js
+++ src/bg/background.js
@@ -6,8 +6,33 @@
 
 
 //example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+// chrome.extension.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//   	chrome.pageAction.show(sender.tab.id);
+//     sendResponse();
+//   });
+
+$(document).ready(function() {
+
+    var Firebase = require("firebase");
+    var myDataRef = new Firebase('https://quickreminders.firebaseio.com/');
+
+
+    
+    // var reminderArray = [];
+    
+    $('#button').click(function() {
+        var reminderText = $('#reminder').val();
+        console.log(reminderText);
+        $('.list').prepend('<div class="item">'+ reminderText+ '</div>');
+
+        // firebase
+        // myDataRef.set('User ' + name + ' says ' + text);
+        // or myDataRef.set({name: name, text: text});
+        // need to get the time the reminder should be sent
+        
+    });
+    $('input').focus(function() {
+        $(this).css("outline-color","#6699FF");
+    });
+});
